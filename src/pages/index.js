1cd31f6970a4a2532a62697bfa95d170d861fa3e
Riley Brown <riley@riley.gg>
2019-09-01 18:53:48 -0400

    added state and logic to index page to persist order to local storage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,33 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import MenuItems from "../components/MenuItems"
+import OrderCart from "../components/OrderCart"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [order, setOrder] = useState([])
+
+  useEffect(() => {
+    const localStorageOrder = localStorage.getItem("order")
+
+    if (localStorageOrder) {
+      setOrder(JSON.parse(localStorageOrder))
+    }
+  }, [])
+
+  const addToOrder = item => {
+    setOrder(order => [...order, item])
+    localStorage.setItem("order", JSON.stringify(order))
+  }
+  return (
+    <Layout>
+      <MenuItems addToOrder={addToOrder} />
+      <OrderCart order={order} />
+    </Layout>
+  )
+}
 
 export default IndexPage
