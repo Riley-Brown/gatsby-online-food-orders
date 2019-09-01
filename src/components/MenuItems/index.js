14c2fdb53b9789a8659e6c5cd083a035f8141a23
Riley Brown <riley@riley.gg>
2019-09-01 14:42:26 -0400

    added products query, import items component to pass query data

--- src/components/MenuItems/index.js
+++ src/components/MenuItems/index.js
@@ -1,10 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import { StyledMenuItems } from "./StyledMenuItems"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Item from "../Item"
 
 export default function MenuItems() {
+  const products = useStaticQuery(graphql`
+    query ProductsQuery {
+      allContentfulMenuItems {
+        edges {
+          node {
+            itemName
+            itemImage {
+              fluid {
+                ...GatsbyContentfulFluid
+              }
+            }
+            itemOptions {
+              itemSizes
+              itemPrices
+              itemAddOns {
+                addOn
+                addOnPrice
+              }
+            }
+            itemDescription {
+              itemDescription
+            }
+            contentful_id
+          }
+        }
+      }
+    }
+  `)
+
+  const { edges } = products.allContentfulMenuItems
+  console.log(edges)
   return (
     <StyledMenuItems>
-      <h1>ayeee</h1>
+      {edges.map(edge => (
+        <Item item={edge.node} />
+      ))}
     </StyledMenuItems>
   )
 }
