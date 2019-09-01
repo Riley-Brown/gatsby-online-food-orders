a26d6cff21c6136b99a96d5207873594a911c968
Riley Brown <riley@riley.gg>
2019-09-01 18:53:24 -0400

    created order cart component

---
+++ src/components/OrderCart/StyledOrderCart.js
@@ -0,0 +1,5 @@
+import styled from "styled-components"
+
+export const StyledOrderCart = styled.div`
+  color: red;
+`
---
+++ src/components/OrderCart/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { StyledOrderCart } from "./StyledOrderCart"
+
+export default function index({ order }) {
+  console.log(order)
+  return (
+    <StyledOrderCart>
+      <h1>ayeee</h1>
+    </StyledOrderCart>
+  )
+}
