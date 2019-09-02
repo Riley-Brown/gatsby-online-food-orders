eab2df63d96713e1b268aaea3bd844ce75060b4c
Riley Brown <riley@riley.gg>
2019-09-02 19:11:35 -0400

    fixed price.toFixed type error bug, added delete icon with functionality

--- src/components/OrderCart/index.js
+++ src/components/OrderCart/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react"
 import { StyledOrderCart } from "./StyledOrderCart"
+import deleteIcon from "../../assets/svg/delete.svg"
 
-export default function OrderCart({ order }) {
+export default function OrderCart({ order, removeFromOrder }) {
   const [totalPrice, setTotalPrice] = useState(null)
 
   useEffect(() => {
@@ -10,34 +11,43 @@ export default function OrderCart({ order }) {
         price: Number(a.price) + Number(b.price),
       }))
 
-      setTotalPrice(total.price)
+      setTotalPrice(Number(total.price).toFixed(2))
     }
   }, [order])
 
   return (
     <StyledOrderCart>
       <h2 className="order-title">Your Order</h2>
-      {order.map(item => (
-        <div className="item">
-          <div className="item-name">
-            <h1>
-              {item.quantity} {item.size} {item.name}
-            </h1>
-            <span>${item.price}</span>
-          </div>
-          {/* Add Ons */}
-          {item.addOns.length > 0 && (
-            <div className="add-ons">
-              <ul>
-                <h4>Add Ons</h4>
-                {item.addOns.map(addOn => (
-                  <li>{addOn}</li>
-                ))}
-              </ul>
+      <div className="items">
+        {order.map((item, index) => (
+          <div className="item">
+            <div className="item-name">
+              <h1>
+                {item.quantity} {item.size} {item.name}
+              </h1>
+              <div>
+                <span>${item.price}</span>
+                <img
+                  onClick={() => removeFromOrder(index)}
+                  src={deleteIcon}
+                  alt="Delete Item"
+                />
+              </div>
             </div>
-          )}
-        </div>
-      ))}
+            {/* Add Ons */}
+            {item.addOns.length > 0 && (
+              <div className="add-ons">
+                <ul>
+                  <h4>Add Ons</h4>
+                  {item.addOns.map(addOn => (
+                    <li>{addOn}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
       <hr />
       <div className="total">
         <h3>Total</h3>
