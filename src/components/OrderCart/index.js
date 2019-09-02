522bdf8aa46b8df305e6c446d5cb255ab6e0931e
Riley Brown <riley@riley.gg>
2019-09-02 14:57:43 -0400

    added dynamic order items to order cart

--- src/components/OrderCart/index.js
+++ src/components/OrderCart/index.js
@@ -2,10 +2,23 @@ import React from "react"
 import { StyledOrderCart } from "./StyledOrderCart"
 
 export default function index({ order }) {
-  console.log(order)
   return (
     <StyledOrderCart>
-      <h1>ayeee</h1>
+      <h2 className="order-title">Your Order</h2>
+      {order.map(item => (
+        <div className="item">
+          <div className="item-name">
+            <h1>
+              {item.quantity} {item.size} {item.name}
+            </h1>
+            <span>${item.price}</span>
+          </div>
+          <h4>AddOns:</h4>
+          {item.addOns.length > 0 && item.addOns.map(addOn => <h4>{addOn}</h4>)}
+        </div>
+      ))}
+      <h3>Total</h3>
+      <span>{order.price}</span>
     </StyledOrderCart>
   )
 }
