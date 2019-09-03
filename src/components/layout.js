4738ce8eacc53713370a470bd5171685414fc9b7
Riley Brown <riley@riley.gg>
2019-09-02 21:09:46 -0400

    remove default gatsby footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,11 +35,11 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
+        {/* <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        </footer> */}
       </div>
     </>
   )
