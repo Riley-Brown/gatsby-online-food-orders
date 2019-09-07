ec25e14456fd39b5305ad345f08fc8ceb33a95a3
Riley Brown <riley@riley.gg>
2019-09-07 17:04:17 -0400

    added ref to get total cart height, added toggle cart state with transform transition

--- src/components/OrderCart/index.js
+++ src/components/OrderCart/index.js
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import { StyledOrderCart } from "./StyledOrderCart"
 import deleteIcon from "../../assets/svg/delete.svg"
+import chevronUp from "../../assets/svg/chevron-up.svg"
+import chevronDown from "../../assets/svg/chevron-down.svg"
 
 export default function OrderCart({ order, removeFromOrder }) {
   const [totalPrice, setTotalPrice] = useState(null)
+  const [showFullCart, setShowFullCart] = useState(true)
+  const [cartHeight, setCartHeight] = useState(null)
 
+  const cartRef = useRef(null)
+
+  // calculate total price every order change
   useEffect(() => {
     if (order.length > 0) {
       const total = order.reduce((a, b) => ({
@@ -16,9 +23,37 @@ export default function OrderCart({ order, removeFromOrder }) {
     }
   }, [order])
 
+  // set cart height every order change
+  useEffect(() => {
+    if (cartRef.current) {
+      setCartHeight(cartRef.current.clientHeight)
+    }
+  }, [order])
+
   return (
-    <StyledOrderCart>
-      <h2 className="order-title">Your Order</h2>
+    <StyledOrderCart
+      ref={cartRef}
+      style={{
+        transform: !showFullCart ? `translateY(${cartHeight - 50}px)` : null,
+      }}
+    >
+      <div
+        className="order-header"
+        onClick={() => !showFullCart && setShowFullCart(true)}
+        style={{ cursor: !showFullCart && "pointer" }}
+      >
+        <h2 className="order-title">Your Order</h2>
+        {showFullCart ? (
+          <img
+            className="toggle"
+            onClick={() => setShowFullCart(false)}
+            src={chevronDown}
+            alt="Minimize Cart"
+          />
+        ) : (
+          <img className="toggle" src={chevronUp} alt="Maximize Cart" />
+        )}
+      </div>
       <div className="items">
         {order.map((item, index) => (
           <div className="item">
@@ -26,7 +61,7 @@ export default function OrderCart({ order, removeFromOrder }) {
               <h1>
                 {item.quantity} {item.size} {item.name}
               </h1>
-              <div>
+              <div className="item-price">
                 <span>${item.price}</span>
                 <img
                   onClick={() => removeFromOrder(index)}
