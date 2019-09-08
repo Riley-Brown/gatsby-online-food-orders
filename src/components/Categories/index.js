f36821ad4037c7d0cff9c3cedcf64739723e932f
Riley Brown <riley@riley.gg>
2019-09-08 17:30:25 -0400

    Added query for categories component

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,10 +1,39 @@
 import React from "react"
-import Category from "./Category"
+import { useStaticQuery, graphql } from "gatsby"
+import { StyledCategories } from "./StyledCategories"
+import Img from "gatsby-image"
+import { Link } from "gatsby"
 
 export default function Categories() {
+  const categories = useStaticQuery(graphql`
+    query CategoriesQuery {
+      allContentfulCategory {
+        edges {
+          node {
+            id
+            categoryName
+            slug
+            categoryImage {
+              fluid {
+                ...GatsbyContentfulFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  const { edges } = categories.allContentfulCategory
   return (
-    <div>
-      <h1>lmaooo ayyy</h1>
-    </div>
+    <StyledCategories>
+      {edges.map(edge => (
+        <div className="category">
+          <Link to={edge.node.slug}>
+            <Img fluid={edge.node.categoryImage.fluid} />
+            <h1>{edge.node.categoryName}</h1>
+          </Link>
+        </div>
+      ))}
+    </StyledCategories>
   )
 }
