1afbabea54d466ce4870dcccfbd40480c20afbfe
Riley Brown <riley@riley.gg>
2019-09-08 14:22:08 -0400

    Added createPages node API to create category pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+// Create Category Pages
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+
+  return new Promise((resolve, reject) => {
+    const categoryTemplate = path.resolve(
+      "src/components/Categories/Category/index.js"
+    )
+    resolve(
+      graphql(`
+        {
+          allContentfulCategory {
+            edges {
+              node {
+                slug
+                categoryName
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+        result.data.allContentfulCategory.edges.forEach(edge => {
+          createPage({
+            path: edge.node.slug,
+            component: categoryTemplate,
+            context: {
+              slug: edge.node.slug,
+              categoryName: edge.node.categoryName,
+            },
+          })
+        })
+        return
+      })
+    )
+  })
+}
