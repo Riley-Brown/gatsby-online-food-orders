1259e66ff2350beeb6dee85be861b32d6f157ed8
Riley Brown <riley@riley.gg>
2019-09-08 17:31:03 -0400

    added query to category component

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,10 +1,53 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { StyledCategory } from "./StyledCategory"
+import Img from "gatsby-image"
 
-export default function Category(props) {
-  console.log(props)
+export default function Category({ data }) {
+  console.log(data)
   return (
-    <div>
-      <h1>Ayee lmao</h1>
-    </div>
+    <StyledCategory>
+      <h1>{data.contentfulCategory.categoryName}</h1>
+    </StyledCategory>
   )
 }
+
+export const category = graphql`
+  query CategoryQuery($slug: String!) {
+    contentfulCategory(slug: { eq: $slug }) {
+      slug
+      categoryName
+      categoryImage {
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+      item {
+        id
+        slug
+        itemPrice
+        inStock
+        itemAddOns {
+          addOnName
+          addOnPrice
+        }
+        itemImage {
+          fluid {
+            src
+          }
+        }
+        itemDescription {
+          id
+          itemDescription
+        }
+        itemOptions {
+          optionName
+          optionChoice {
+            choiceName
+            choicePrice
+          }
+        }
+      }
+    }
+  }
+`
