17f280e3507d3ef4ca2ba413ffe7cec249dbce14
Riley Brown <riley@riley.gg>
2019-09-08 23:12:32 -0400

    added layout component and add styled container div

--- src/components/Category/StyledCategory.js
+++ src/components/Category/StyledCategory.js
@@ -3,4 +3,10 @@ import styled from "styled-components"
 export const StyledCategory = styled.div`
   display: flex;
   flex-wrap: wrap;
+  justify-content: space-between;
+  .category-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
 `
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -3,16 +3,21 @@ import { graphql } from "gatsby"
 import { StyledCategory } from "./StyledCategory"
 import Img from "gatsby-image"
 import Item from "../Item"
+import Layout from "../layout"
 
 export default function Category({ data }) {
   console.log(data)
   return (
-    <StyledCategory>
-      <h1>{data.contentfulCategory.categoryName}</h1>
-      {data.contentfulCategory.item.map(item => (
-        <Item item={item} />
-      ))}
-    </StyledCategory>
+    <Layout>
+      <StyledCategory>
+        <h1>{data.contentfulCategory.categoryName}</h1>
+        <div className="category-container">
+          {data.contentfulCategory.item.map(item => (
+            <Item item={item} />
+          ))}
+        </div>
+      </StyledCategory>
+    </Layout>
   )
 }
 
