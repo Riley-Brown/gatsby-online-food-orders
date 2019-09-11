f37cd243ecd2138766e4c3d04ac86aa52f1b15d4
Riley Brown <riley@riley.gg>
2019-09-10 22:39:43 -0400

    move add ons logic into seperate component

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -2,23 +2,14 @@ import React, { useState, useEffect, useRef } from "react"
 import Img from "gatsby-image"
 import { StyledItem } from "./StyledItem"
 
-import plusSvg from "../../assets/svg/plus.svg"
-import minusSvg from "../../assets/svg/minus.svg"
+import AddOns from "./AddOns"
 
 export default function Item({ item, addToOrder }) {
   const [price, setPrice] = useState(null)
   const [totalPrice, setTotalPrice] = useState(null)
   const [itemSize, setItemSize] = useState(null)
   const [addOnsPrice, setAddOnsPrice] = useState(null)
-  const [addOns, setAddOns] = useState([])
   const [quantity, setQuantity] = useState(1)
-  const [showAddOns, setShowAddOns] = useState(false)
-  const [addOnsHeight, setAddOnsHeight] = useState(null)
-  const addOnsRef = useRef(null)
-
-  useEffect(() => {
-    setAddOnsHeight(addOnsRef.current.clientHeight)
-  }, [addOnsRef])
 
   // handle initial price
   useEffect(() => {
@@ -42,7 +33,7 @@ export default function Item({ item, addToOrder }) {
       }
     }
     handleTotalPrice()
-  }, [quantity, itemSize, addOns])
+  }, [quantity, itemSize, addOnsPrice])
 
   const handleSizeChange = e => {
     const dataset = e.target.options[e.target.selectedIndex].dataset
@@ -50,24 +41,7 @@ export default function Item({ item, addToOrder }) {
     setItemSize(dataset.size)
   }
 
-  const handleAddOns = e => {
-    const { price } = e.target.dataset
-    const addOn = e.target.value
-    if (e.target.checked) {
-      setAddOnsPrice(prevPrice =>
-        prevPrice !== null ? prevPrice + Number(price) : Number(price)
-      )
-      setAddOns(addOns => [...addOns, addOn])
-    } else if (!e.target.checked) {
-      setAddOnsPrice(prevPrice =>
-        prevPrice !== null ? Number(prevPrice) - Number(price) : Number(price)
-      )
-      const filteredAddOns = addOns.filter(addOn => addOn !== e.target.value)
-      setAddOns(filteredAddOns)
-    }
-  }
-
-  const handleAddToOrder = () => {
+  const handleAddToOrder = addOns => {
     addToOrder({
       name: item.itemName,
       price: totalPrice,
@@ -113,41 +87,14 @@ export default function Item({ item, addToOrder }) {
 
       {/* Add ons */}
       {item.itemAddOns && (
-        <div className="add-ons-container">
-          <h5 onClick={() => setShowAddOns(show => !show)}>
-            Add Ons
-            <img src={showAddOns ? minusSvg : plusSvg} alt="" />
-          </h5>
-          <div
-            className="add-ons"
-            style={{
-              height:
-                addOnsHeight && showAddOns
-                  ? addOnsHeight
-                  : addOnsHeight
-                  ? "0"
-                  : null,
-              visibility: !addOnsHeight && "hidden",
-            }}
-            ref={addOnsRef}
-          >
-            {item.itemAddOns.map(addOn => (
-              <label htmlFor={`${item.itemName}-${addOn.addOnName}`}>
-                <input
-                  type="checkbox"
-                  name={addOn.addOnName}
-                  data-price={addOn.addOnPrice}
-                  id={`${item.itemName}-${addOn.addOnName}`}
-                  value={addOn.addOnName}
-                  onChange={handleAddOns}
-                />
-                <span className="check-mark" />
-                <span>{addOn.addOnName}</span>
-                <span>+${addOn.addOnPrice.toFixed(2)}</span>
-              </label>
-            ))}
-          </div>
-        </div>
+        <AddOns
+          addOns={item.itemAddOns}
+          item={item}
+          quantity={quantity}
+          setTotalPrice={setTotalPrice}
+          itemPrice={price}
+          setAddOnsPrice={setAddOnsPrice}
+        />
       )}
       <button className="add-to-order">Add to Order</button>
     </StyledItem>
