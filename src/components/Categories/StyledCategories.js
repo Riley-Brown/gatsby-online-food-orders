b8b91e327b2bec2f67a4d7b7f82a9ae73bff95a8
Riley Brown <riley@riley.gg>
2019-09-11 20:53:36 -0400

    adjusted categories styles

--- src/components/Categories/StyledCategories.js
+++ src/components/Categories/StyledCategories.js
@@ -1,23 +1,39 @@
 import styled from "styled-components"
 
 export const StyledCategories = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
   .category {
     background: #f5f5f5;
     margin-bottom: 4rem;
     border-radius: 15px;
+    width: 20%;
+    position: relative;
+    background: rgba(0, 0, 0, 0.4);
+    &:hover {
+      h1 {
+        transform: translate(-50%, -65%);
+      }
+    }
     a {
       text-decoration: none;
       align-items: center;
-      display: flex;
       .gatsby-image-wrapper {
-        flex: 0 0 20%;
-        border-top-left-radius: 15px;
-        border-bottom-left-radius: 15px;
+        width: 100%;
+        border-radius: 15px;
+        z-index: -1;
       }
       h1 {
         font-size: 3rem;
         color: #333;
-        margin-left: 2rem;
+        position: absolute;
+        color: #fff;
+        left: 50%;
+        top: 50%;
+        font-size: 3.5rem;
+        transform: translate(-50%, -50%);
+        transition: 200ms;
       }
     }
   }
