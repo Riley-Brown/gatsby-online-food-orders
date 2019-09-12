5f51cc537940d552c009b16d160d0c98e6e633d2
Riley Brown <riley@riley.gg>
2019-09-11 23:20:36 -0400

    set proper width and fixed space around last item bug

--- src/components/Category/StyledCategory.js
+++ src/components/Category/StyledCategory.js
@@ -1,12 +1,14 @@
 import styled from "styled-components"
 
 export const StyledCategory = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+  flex: 0 0 75%;
   .category-container {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
+    &::after {
+      content: "";
+      width: 40%;
+    }
   }
 `
