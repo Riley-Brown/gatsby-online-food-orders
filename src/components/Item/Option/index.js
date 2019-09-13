87e330fdff94eaa521cb81f03c272793803b3a55
Riley Brown <riley@riley.gg>
2019-09-13 00:12:54 -0400

    added map for option name and option choices

--- src/components/Item/Option/index.js
+++ src/components/Item/Option/index.js
@@ -1,17 +1,54 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
+
 import { StyledOption } from "./StyledOption"
 
-export default function Option({ option }) {
-  console.log(option)
+export default function Option({ option, showOptions }) {
+  // const [show, setShow] = useState(false)
+  const [optionHeight, setOptionHeight] = useState(null)
+  const [choice, setChoice] = useState([])
+
+  const optionRef = useRef(null)
+
+  useEffect(() => {
+    setOptionHeight(optionRef.current.clientHeight)
+  }, [optionRef])
+
+  const handleOption = e => {
+    console.log("lol")
+  }
+
   return (
     <StyledOption>
-      <h1>{option.optionName}</h1>
-      {option.optionChoice.map(choice => (
-        <div>
-          <h4>{choice.choiceName}</h4>
-          <span>{choice.choicePrice}</span>
-        </div>
-      ))}
+      <div
+        className="option"
+        style={{
+          height:
+            optionHeight && showOptions
+              ? optionHeight
+              : optionHeight
+              ? "0"
+              : null,
+          visibility: !optionHeight && "hidden",
+        }}
+        ref={optionRef}
+      >
+        <h1>{option.optionName}</h1>
+        {option.optionChoice.map(choice => (
+          <label htmlFor={`${option.optionName}-${choice.choiceName}`}>
+            <input
+              type="radio"
+              name="radio"
+              data-price={choice.choicePrice}
+              id={`${option.optionName}-${choice.choiceName}`}
+              value={choice.choiceName}
+              onChange={handleOption}
+            />
+            <span className="check-mark" />
+            <span>{choice.choiceName}</span>
+            <span>+${choice.choicePrice.toFixed(2)}</span>
+          </label>
+        ))}
+      </div>
     </StyledOption>
   )
 }
