b4251f132de61b65c0639dc6cc295a5d460f46ad
Riley Brown <riley@riley.gg>
2019-09-13 00:27:08 -0400

    adjusted spacing for option

--- src/components/Item/Option/StyledOption.js
+++ src/components/Item/Option/StyledOption.js
@@ -4,5 +4,10 @@ export const StyledOption = styled.div`
   .option {
     overflow: hidden;
     transition: height 300ms;
+    margin-top: 1rem;
+    h5 {
+      margin-bottom: 1rem;
+      font-weight: 500;
+    }
   }
 `
