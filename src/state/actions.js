ec5182a9045f849b438a5a64bac5fa0b8a3d06ac
Riley Brown <riley@riley.gg>
2019-09-29 14:58:25 -0400

    created actions file

---
+++ src/state/actions.js
@@ -0,0 +1,11 @@
+export const SET_SHOW_CART = "SET_SHOW_CART"
+export const setShowCart = payload => ({
+  type: SET_SHOW_CART,
+  payload,
+})
+
+export const SET_ORDER = "SET_ORDER"
+export const setOrder = payload => ({
+  type: SET_ORDER,
+  payload,
+})
