4d96d7ca37b883ab54e3bb4a853839cdfcdc4af1
Riley Brown <riley@riley.gg>
2019-10-05 22:48:29 -0400

    added functions scope to remove unneeded dispatch return

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,27 +1,31 @@
 export const SET_SHOW_CART = "SET_SHOW_CART"
-export const setShowCart = payload => dispatch =>
+export const setShowCart = payload => dispatch => {
   dispatch({
     type: SET_SHOW_CART,
     payload,
   })
+}
 
 export const SET_ORDER = "SET_ORDER"
-export const setOrder = payload => dispatch =>
+export const setOrder = payload => dispatch => {
   dispatch({
     type: SET_ORDER,
     payload,
   })
+}
 
 export const ADD_TO_ORDER = "ADD_TO_ORDER"
-export const addToOrder = payload => dispatch =>
+export const addToOrder = payload => dispatch => {
   dispatch({
     type: ADD_TO_ORDER,
     payload,
   })
+}
 
 export const REMOVE_FROM_ORDER = "REMOVE_FROM_ORDER"
-export const removeFromOrder = payload => dispatch =>
+export const removeFromOrder = payload => dispatch => {
   dispatch({
     type: REMOVE_FROM_ORDER,
     payload,
   })
+}
