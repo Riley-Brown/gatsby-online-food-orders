f8e9616ecafc59b2249df06733de87a184c9d7f3
Riley Brown <riley@riley.gg>
2019-10-06 13:03:45 -0400

    adjusted set order action

--- src/state/global.js
+++ src/state/global.js
@@ -16,7 +16,7 @@ export default (state = initialState, action) => {
     case SET_SHOW_CART:
       return { ...state, show: payload }
     case SET_ORDER:
-      return { ...state, order: [...state.order, ...payload] }
+      return { ...state, order: [...payload] }
     case ADD_TO_ORDER:
       return { ...state, order: [...state.order, payload] }
     case REMOVE_FROM_ORDER:
