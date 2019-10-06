476190eac3f6f535957049cd6c02917eca976650
Riley Brown <riley@riley.gg>
2019-10-05 22:11:46 -0400

    added redux logger to redux wrapper

--- src/state/ReduxWrapper.js
+++ src/state/ReduxWrapper.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux"
 import { createStore, compose, applyMiddleware } from "redux"
 import rootReducer from "./rootReducer"
 import thunk from "redux-thunk"
+import logger from "redux-logger"
 
 let store
 
@@ -12,9 +13,12 @@ if (process.env.NODE_ENV === "development" && typeof window !== "undefined") {
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-  store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+  store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk, logger))
+  )
 } else {
-  store = createStore(rootReducer)
+  store = createStore(rootReducer, applyMiddleware(thunk))
 }
 
 export default ({ element }) => <Provider store={store}>{element}</Provider>
