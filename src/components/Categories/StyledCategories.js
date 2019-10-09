77a945b7bd003b1ab659884f6fb2057248ac2c9b
Riley Brown <riley@riley.gg>
2019-10-08 22:37:47 -0400

    categories media queries

--- src/components/Categories/StyledCategories.js
+++ src/components/Categories/StyledCategories.js
@@ -7,17 +7,32 @@ export const StyledCategories = styled.div`
   flex: ${props => (props.showCart ? "0 1 75%" : "1 1 100%")};
   transition: 300ms;
   justify-content: space-around;
+  width: 100%;
+  @media (max-width: 960px) {
+    flex: 0 1 100%;
+  }
   &::after {
     content: "";
     width: 40%;
+    margin: 0 10px 4rem;
+    min-width: 370px;
   }
   .category {
     background: #f5f5f5;
-    margin-bottom: 4rem;
     border-radius: 15px;
     width: 40%;
     position: relative;
     background: rgba(0, 0, 0, 0.4);
+    min-width: 370px;
+    margin: 0 10px 4rem;
+
+    @media (max-width: 800px) {
+      width: 80%;
+    }
+    @media (max-width: 500px) {
+      min-width: initial;
+      width: 100%;
+    }
     &:hover {
       h1 {
         transform: translate(-50%, -65%);
