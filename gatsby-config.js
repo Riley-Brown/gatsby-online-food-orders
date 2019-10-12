9f46d6a52ecb29c7e4ff7cc0ceb30810139b9424
Riley Brown <riley@riley.gg>
2019-10-12 18:01:45 -0400

    update pwa config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Rawberri`,
+        short_name: `Rawberri`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `standalone`,
+        icon: `src/assets/svg/rawberri-logo.png`, // This path is relative to the root of the site.
       },
     },
     {
