d3fa3287dc16133e6d24a9c60f3a6811effe0df4
Riley Brown <riley@riley.gg>
2019-10-12 18:07:18 -0400

    update wrong path to logo 🤷‍

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `standalone`,
-        icon: `src/assets/svg/rawberri-logo.png`, // This path is relative to the root of the site.
+        icon: `src/assets/img/rawberri-logo.png`, // This path is relative to the root of the site.
       },
     },
     {
