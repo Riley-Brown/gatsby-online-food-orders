ed76bf47c4df828caff21efdf7f1d063ba8fefc3
Riley Brown <riley@riley.gg>
2019-10-13 17:45:55 -0400

    add sendgrid lambda function code

--- functions/confirmOrder.js
+++ functions/confirmOrder.js
@@ -1,7 +1,25 @@
-exports.handler = function(event, context, callback) {
-  console.log(event)
-  callback(null, {
-    statusCode: 200,
-    body: "Hello World",
-  })
+const sgMail = require("@sendgrid/mail")
+
+exports.handler = async function(event, context, callback) {
+  sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
+
+  const msg = {
+    to: event.user_email,
+    from: "orders-testing@riley.gg",
+    subject: "New online order placed",
+    html: event.html,
+  }
+
+  if (event.user_email) {
+    await sgMail.send(msg)
+    return {
+      statusCode: 200,
+      body: "success",
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: "something went wrong gg im switching to heroku",
+    }
+  }
 }
