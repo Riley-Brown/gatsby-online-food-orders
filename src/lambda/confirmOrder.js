7b55c8db2fda18ca45b4a2c02b23d337d25b0d14
Riley Brown <riley@riley.gg>
2019-10-13 22:44:29 -0400

    added lambda function to src folder

---
+++ src/lambda/confirmOrder.js
@@ -0,0 +1,37 @@
+const sgMail = require("@sendgrid/mail")
+// const apiKey = process.env.SEND_GRID_API_KEY
+
+exports.handler = function(event, context, callback) {
+  const { SEND_GRID_API_KEY } = process.env
+  sgMail.setApiKey(SEND_GRID_API_KEY)
+  const data = JSON.parse(event.body)
+  // console.log(data)
+
+  console.log(process.env.NODE_ENV)
+
+  const msg = {
+    to: data.user_email,
+    from: "orders-testing@riley.gg",
+    subject: "New online order placed",
+    html: data.html,
+  }
+
+  // callback(null, {
+  //   statusCode: 200,
+  //   body: JSON.stringify({ success: true }),
+  // })
+
+  console.log(event.origin)
+  if (data.user_email) {
+    sgMail.send(msg)
+    return callback(null, {
+      statusCode: 200,
+      body: "success",
+    })
+  } else {
+    return callback(null, {
+      statusCode: 400,
+      body: "something went wrong gg im switching to heroku",
+    })
+  }
+}
