4660b7b40a88eb6de2a6c6f05a54047dbc4f53fd
Riley Brown <riley@riley.gg>
2019-10-16 23:59:56 -0400

    added useSelector for confirm order conditional render

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
+import { useSelector } from "react-redux"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import OrderCart from "../components/OrderCart"
-import Categories from "../components/Categories"
+import Layout from "components/layout"
+import Image from "components/image"
+import SEO from "components/seo"
+import OrderCart from "components/OrderCart"
+import Categories from "components/Categories"
+import ConfirmOrder from "components/ConfirmOrder"
 
 const IndexPage = () => {
+  const showConfirmOrder = useSelector(state => state.global.showConfirmOrder)
+
   return (
     <Layout>
       <Categories />
       <OrderCart />
+      {showConfirmOrder && <ConfirmOrder />}
     </Layout>
   )
 }
