6c1cf7d07ae70b256d30506824c6ec025ef68c1f
Riley Brown <riley@riley.gg>
2019-10-20 01:00:38 -0400

    remove body target selector, centered header content

--- src/components/ConfirmOrder/CustomerInfo/StyledCustomerInfo.js
+++ src/components/ConfirmOrder/CustomerInfo/StyledCustomerInfo.js
@@ -2,33 +2,34 @@ import styled from "styled-components"
 
 export const StyledCustomerInfo = styled.div`
   .header {
-    h4 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    h2 {
       font-size: 2.5rem;
     }
   }
-  .body {
-    form {
-      div {
-        display: flex;
-        padding: 1rem 25px;
-        flex-direction: column;
-        margin-bottom: 1rem;
-        label {
-          margin: 1rem 0;
-        }
-        input {
-          padding: 10px;
-          border: 1px solid #ccc;
-          border-radius: 4px;
-        }
-        textarea {
-          resize: none;
-          padding: 10px;
-          font-size: 1.6rem;
-          font-family: inherit;
-          border-radius: 4px;
-          border: 1px solid #ccc;
-        }
+  form {
+    div {
+      display: flex;
+      padding: 1rem 25px;
+      flex-direction: column;
+      margin-bottom: 1rem;
+      label {
+        margin: 1rem 0;
+      }
+      input {
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+      }
+      textarea {
+        resize: none;
+        padding: 10px;
+        font-size: 1.6rem;
+        font-family: inherit;
+        border-radius: 4px;
+        border: 1px solid #ccc;
       }
     }
   }
